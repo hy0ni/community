c6877ebde3c11c716ac376e28b86c568bd07a4a1
hyoni <jih70004@gmail.com>
2024-09-24 15:40:29 +0900

    헤더 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './page/Home';
+import Header from './components/Header';
+import Signup from './components/Signup';
+import './resource/css/main.min.css';
+
+
 function App() {
-  return
+  return (
+    <Router>
+      <div className='wrap'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import { auth } from '../firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
+function Header() {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  // Firebase의 로그인 상태 변화를 확인
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = () => {
+    signOut(auth);
+  };
+
+  return (
+    <header className='header'>
+      <h1 className='title'>커뮤니티</h1>
+      <nav>
+        {user ? (
+          <>
+            <h2>Welcome, {user.email}</h2>
+            <button className='btn' onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <button className='btn' onClick={() => navigate('/login')} aria-label="로그인 페이지로 이동">로그인</button>
+        )}
+      </nav>
+    </header>
+  )
+}
+export default Header;
