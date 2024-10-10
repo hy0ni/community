d4440c64c2257ec67a8e736c880f7f95dbcae212
hyoni <jih70004@gmail.com>
2024-10-10 16:46:10 +0900

    인기 순, 평점, 순 정렬 셀렉트 박스 추가

--- src/components/NowPlayingMovies.js
+++ src/components/NowPlayingMovies.js
@@ -4,14 +4,16 @@ import MovieCard from './MovieCard';
 
 function NowPlayingMovies() {
   const [movies, setMovies] = useState([]);
-  const [page, setPage] = useState(1); // 현재 페이지 번호 상태 관리
+  const [page, setPage] = useState(1);
+  const [sortBy, setSortBy] = useState('popularity');
   const [error, setError] = useState(null);
 
   const getNowPlayingMovies = async (page) => {
     try {
       const data = await fetchNowPlayingMovies('ko-KR', page);
-      const sortedMovies = data.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
-      setMovies(sortedMovies);
+      // const sortedMovies = data.sort((a, b) => b.vote_average - a.vote_average);
+      setMovies(data);
+      console.log(data);
     } catch (error) {
       setError('현재 상영중인 영화를 로드하는데 실패했습니다.')
     }
@@ -21,6 +23,19 @@ function NowPlayingMovies() {
     getNowPlayingMovies(page);
   }, [page])
 
+
+  // 인기 순 정렬
+  const sortByPopularity = (movies) => {
+    return movies.sort((a, b) => b.popularity - a.popularity);
+  };
+
+  // 평점 순 정렬
+  const sortByVoteAverage = (movies) => {
+    return movies.sort((a, b) => b.vote_average - a.vote_average);
+  };
+
+  const sortedMovies = sortBy === 'popularity' ? sortByPopularity(movies) : sortByVoteAverage(movies);
+
   if (error) {
     return <div>Error: {error.message}</div>
   }
@@ -29,12 +44,20 @@ function NowPlayingMovies() {
     <div>
       <h2>현재 상영 중인 영화</h2>
       <ul className='movie-list'>
-        {movies.map(movie => (
+        {sortedMovies.map(movie => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </ul>
-      <div className='btn-wrap'>
-        <button onClick={() => setPage(prevPage => Math.max(prevPage - 1, 1))} disabled={page === 1}>이전 페이지</button>
+      <div className='sorted-wrap'>
+        <select onChange={(e) => setSortBy(e.target.value)} defaultValue="popularity">
+          <option value="popularity">인기순</option>
+          <option value="vote_average">평점순</option>
+        </select>
+      </div>
+      <div className='pagination'>
+        <button onClick={() => setPage(prevPage => Math.max(prevPage - 1, 1))} disabled={page === 1}>
+          이전 페이지
+        </button>
         <button onClick={() => setPage(prevPage => prevPage + 1)}>
           다음 페이지
         </button>
